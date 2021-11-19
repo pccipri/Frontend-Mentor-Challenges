c7b32f42800604136f757dde0ecd564de82752f6
pccipri <popandreiciprian1@gmail.com>
2021-11-19 16:25:50 +0200

    Now the triangle is hidden in mobile view.

--- Article-preview-component-master/script.js
+++ Article-preview-component-master/script.js
@@ -3,30 +3,39 @@ const shareContainer = document.getElementById('shareContainer');
 const shareImage = document.getElementById('shareImage')
 const shareTriangle = document.getElementById('shareTriangle');
 
-if (window.innerWidth > 769) {
-shareButton.addEventListener('mouseenter', (event) => {
-    shareContainer.classList.add('active');
-    shareTriangle.classList.add('active');
-    shareImage.style.filter = 'brightness(0) invert(1)';
-  });
-
-  shareButton.addEventListener('mouseleave', (event) => {
-    shareContainer.classList.remove('active');
-    shareTriangle.classList.remove('active');
-    shareImage.style.filter = 'brightness(100%) invert(0)';
-  });
+function addHighlight(button, image) {
+  button.style.backgroundColor = 'hsl(214, 17%, 51%)';
+  image.style.filter = 'brightness(0) invert(1)';
 }
-else {
-  shareButton.addEventListener('touchstart', (event) => {
-    if(shareContainer.classList.contains('active')) {
-      shareContainer.classList.remove('active');
-      shareButton.style.backgroundColor = 'hsl(210, 46%, 95%)';
-      shareImage.style.filter = 'brightness(100%) invert(0)';
-    } else {
-      shareContainer.classList.add('active');
-      shareButton.style.backgroundColor = 'hsl(214, 17%, 51%)';
-      shareImage.style.filter = 'brightness(0) invert(1)';
-    }
-  });
 
+function removeHighlight(button, image) {
+  button.style.backgroundColor = 'hsl(210, 46%, 95%)';
+  image.style.filter = 'brightness(100%) invert(0)';
 }
+
+function addActiveClass(element) {
+  element.classList.add('active');
+}
+
+function removeActiveClass(element) {
+  element.classList.remove('active');
+}
+
+shareButton.addEventListener('mouseenter', () => {
+  if(window.innerWidth > 768) {
+  addActiveClass(shareContainer);
+  addActiveClass(shareTriangle);
+  addHighlight(shareButton, shareImage);
+  }
+  else {
+    addActiveClass(shareContainer);
+    addHighlight(shareButton, shareImage);
+  }
+
+});
+
+shareButton.addEventListener('mouseleave', () => {
+  removeActiveClass(shareContainer);
+  removeActiveClass(shareTriangle);
+  removeHighlight(shareButton, shareImage);
+})
